a5b32fce667ce33b8f31135c35db0f35d2c85799
Javier Huerta Jr <javsteratii@gmail.com>
2019-09-07 19:32:09 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,3 @@
-# Write your #display_rainbow method here
+def display_rainbow(cheese)
+   puts "R: #{cheese[0]}, O: #{cheese[1]}, Y: #{cheese[2]}, G: #{cheese[3]}, B: #{cheese[4]}, I: #{cheese[5]}, V: #{cheese[6]}"
+end
